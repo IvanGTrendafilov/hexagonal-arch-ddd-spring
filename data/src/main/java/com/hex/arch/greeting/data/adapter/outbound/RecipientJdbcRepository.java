56aebd14e922ddaf12efd23e4bf69093d4aec23f
Cheshmedzhiev1 <martincheshmedzhiev@gmail.com>
2026-01-25 12:26:22 +0000

    c

--- data/src/main/java/com/hex/arch/greeting/data/adapter/outbound/RecipientJdbcRepository.java
+++ data/src/main/java/com/hex/arch/greeting/data/adapter/outbound/RecipientJdbcRepository.java
@@ -2,11 +2,13 @@ package com.hex.arch.greeting.data.adapter.outbound;
 
 import com.hex.arch.greeting.domain.model.Recipient;
 import com.hex.arch.greeting.domain.repository.RecipientRepository;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
+
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -23,15 +25,15 @@ public class RecipientJdbcRepository implements RecipientRepository {
     public void save(Recipient recipient) {
         jdbcClient
                 .sql("""
-                INSERT INTO recipients (id, first_name, last_name, created_at, address, gender, age)
-                VALUES (:id, :firstName, :lastName, :createdAt, :address, :gender, :age)
-                ON CONFLICT (id) DO UPDATE SET
-                    first_name = EXCLUDED.first_name,
-                    last_name = EXCLUDED.last_name,
-                    address = EXCLUDED.address,
-                    gender = EXCLUDED.gender,
-                    age = EXCLUDED.age
-                """)
+                        INSERT INTO recipients (id, first_name, last_name, created_at, address, gender, age)
+                        VALUES (:id, :firstName, :lastName, :createdAt, :address, :gender, :age)
+                        ON CONFLICT (id) DO UPDATE SET
+                            first_name = EXCLUDED.first_name,
+                            last_name = EXCLUDED.last_name,
+                            address = EXCLUDED.address,
+                            gender = EXCLUDED.gender,
+                            age = EXCLUDED.age
+                        """)
                 .param("id", recipient.id())
                 .param("firstName", recipient.firstName())
                 .param("lastName", recipient.lastName())
@@ -47,7 +49,20 @@ public class RecipientJdbcRepository implements RecipientRepository {
         return jdbcClient
                 .sql("SELECT id, first_name, last_name, created_at, address, gender, age FROM recipients WHERE id = :id")
                 .param("id", id)
-                .query(new RecipientRowMapper())
+                .query(new RowMapper<Recipient>() {
+                    @Override
+                    public Recipient mapRow(ResultSet rs, int rowNum) throws SQLException {   //anonymous class
+                        return new Recipient(
+                                UUID.fromString(rs.getString("id")),
+                                rs.getString("first_name"),
+                                rs.getString("last_name"),
+                                rs.getTimestamp("created_at").toInstant(),
+                                rs.getString("address"),
+                                rs.getString("gender"),
+                                rs.getInt("age")
+                        );
+                    }
+                })
                 .optional();
     }
 
@@ -57,20 +72,5 @@ public class RecipientJdbcRepository implements RecipientRepository {
                 .sql("DELETE FROM recipients WHERE id = :id")
                 .param("id", id)
                 .update();
-    }
-
-    private static class RecipientRowMapper implements RowMapper<Recipient> {
-        @Override
-        public Recipient mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return new Recipient(
-                    UUID.fromString(rs.getString("id")),
-                    rs.getString("first_name"),
-                    rs.getString("last_name"),
-                    rs.getTimestamp("created_at").toInstant(),
-                    rs.getString("address"),
-                    rs.getString("gender"),
-                    rs.getInt("age")
-            );
-        }
     }
 }
